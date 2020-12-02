c3ed3901b41f0a33ac1b9b900f10627420e562a8
Matt Hughes <zeepkdb@gmail.com>
2020-12-02 08:43:16 -0800

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
-import React, { useState } from 'react'
-import './App.css'
-import Header from './components/Header.js'
-import LocalStorage from './components/LocalStorage.js'
-import Footer from './components/Footer.js'
-import Routing from './components/Routing.js'
-import { BrowserRouter as Router } from 'react-router-dom'
+import React, { useState } from 'react';
+import './App.css';
+import Header from './components/Header.js';
+import LocalStorage from './components/LocalStorage.js';
+import Footer from './components/Footer.js';
+import Routing from './components/Routing.js';
+import { BrowserRouter } from 'react-router-dom';
 
-import 'primereact/resources/themes/nova-light/theme.css'
-import 'primereact/resources/primereact.min.css'
-import 'primeicons/primeicons.css'
-import ReactGA from 'react-ga'
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import ReactGA from 'react-ga';
 
 function initializeReactGA() {
-	ReactGA.initialize('UA-165301764-1')
-	ReactGA.pageview('/homepage')
+	ReactGA.initialize('UA-165301764-1');
+	ReactGA.pageview('/homepage');
 }
-initializeReactGA()
+initializeReactGA();
 
 function App() {
-	const [hideCaught, toggleCaught] = useState(false)
+	const [hideCaught, toggleCaught] = useState(false);
 
 	return (
 		<div className="App">
-			<Router>
+			<BrowserRouter>
 				<LocalStorage />
 				<Header
 					toggleCaught={() => {
-						toggleCaught(!hideCaught)
+						toggleCaught(!hideCaught);
 					}}
 				/>
 				<Routing hideCaught={hideCaught} />
 				<Footer />
-			</Router>
+			</BrowserRouter>
 		</div>
-	)
+	);
 }
 
-export default App
+export default App;
